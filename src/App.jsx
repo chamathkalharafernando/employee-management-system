41580e8a69307ab9733754929ba4fedc390b9df7
Chamath Fernando <chamadula555@gmail.com>
2024-11-09 19:42:04 +0530

    Initial commit: Employee Management System setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from './components/Login'  // Add this import
+
+function App() {
+  return (
+    <Login />
+  )
+}
+
+export default App
